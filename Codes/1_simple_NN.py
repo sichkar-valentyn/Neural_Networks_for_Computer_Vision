3e0d71b9288d82a8ccfcf4d4d402cf971ebd15c0
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2018-07-21 12:29:46 +0300

    Update 1_simple_NN.py

--- Codes/1_simple_NN.py
+++ Codes/1_simple_NN.py
@@ -32,7 +32,8 @@ class NN():
         # Neuron has three inputs and one output
         # Initializing weights to 3 by 1 matrix
         # The values of the weights are in range from -1 to 1
-        self.weights_of_synapses = 2 * np.random.random((3, 1)) - 1  # 3x1 matrix of weights, 3 inputs and 1 output
+        # We receive matrix 3x1 of weights (3 inputs and 1 output)
+        self.weights_of_synapses = 2 * np.random.random((3, 1)) - 1
 
     # Creating function for normalizing weights and other results by Sigmoid curve
     def normalizing_results(self, x):
@@ -49,6 +50,7 @@ class NN():
         # Giving NN the set of input matrices
         # With 'numpy' function 'dot' we multiply set of input matrices to weights
         # Result is returned in normalized form
+        # We multiply matrix 4x3 of inputs on matrix 3x1 of weights and receive matrix 4x1 of outputs
         return self.normalizing_results(np.dot(set_of_inputs, self.weights_of_synapses))
 
     # Creating function for training the NN
@@ -56,9 +58,11 @@ class NN():
         # Training NN desired number of times
         for i in range(iterations):
             # Feeding our NN with training set and calculating output
+            # We multiply matrix 4x3 of inputs on matrix 3x1 of weights and receive matrix 4x1 of outputs
             nn_output = self.run_nn(set_of_inputs_for_training)
 
             # Calculating an error which is the difference between desired output and obtained output
+            # We subtract matrix 4x1 of received outputs from matrix 4x1 of desired outputs
             nn_error = set_of_outputs_for_training - nn_output
 
             # Calculating correction values for weights
@@ -66,9 +70,12 @@ class NN():
             # In this way, the weights that do not fit too much will be corrected more
             # If some inputs are equal to 0, that will not influence to the value of weights
             # We use here function 'T' that transpose matrix and allows to multiply matrices
+            # We multiply transposed matrix 4x3 of inputs (matrix.T = matrix 3x4) on matrix 4x1 of corrections
+            # And receive matrix 3x1 of corrections
             corrections = np.dot(set_of_inputs_for_training.T, nn_error * self.derivative_of_sigmoid(nn_output))
 
             # Implementing corrections of weights
+            # We add matrix 3x1 of current weights with matrix 3x1 of corrections and receive matrix 3x1 of new weights
             self.weights_of_synapses += corrections
 
 
@@ -76,6 +83,7 @@ class NN():
 single_neuron_neural_network = NN()
 
 # Showing the weights of synapses initialized from the very beginning randomly
+# We create here matrix 3x1 of weights
 print(single_neuron_neural_network.weights_of_synapses)
 print()
 # [[-0.16595599]
@@ -84,7 +92,9 @@ print()
 
 # Creating a set of inputs and outputs for the training process
 # We use here function 'array' of the 'numpy' library
+# We create here matrix 4x3
 input_set_for_training = np.array([[1, 1, 1], [1, 0, 1], [0, 0, 1], [0, 1, 1]])
+# We create here matrix 1x4 and transpose it to matrix 4x1 at the same time
 output_set_for_training = np.array([[1, 1, 0, 0]]).T
 
 # Starting the training process with data above and number of repetitions of 5000
