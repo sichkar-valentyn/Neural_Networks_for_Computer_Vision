e34dad718c937caeae91f91c1ec8dd71af2572d2
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2018-10-05 15:43:41 +0300

    Update datasets_preparing.py

--- Codes/Image_Classification/helper_functions/datasets_preparing.py
+++ Codes/Image_Classification/helper_functions/datasets_preparing.py
@@ -1,3 +1,17 @@
+# File: datasets_preparing.py
+# Description: Neural Networks for computer vision in autonomous vehicles and robotics
+# Environment: PyCharm and Anaconda environment
+#
+# MIT License
+# Copyright (c) 2018 Valentyn N Sichkar
+# github.com/sichkar-valentyn
+#
+# Reference to:
+# Valentyn N Sichkar. Neural Networks for computer vision in autonomous vehicles and robotics // GitHub platform. DOI: 10.5281/zenodo.1317904
+
+
+
+
 # Preparing datasets for further using
 # Loading all batches and concatenating them all together
 # Plotting first 100 examples of images from 10 different classes
